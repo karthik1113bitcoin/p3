c0591f5411a914bf118fe6d3c3e1a55493ab8513
karthik1113bitcoin <50091765+karthik1113bitcoin@users.noreply.github.com>
2019-06-20 15:33:14 +0800

    Update reorg.sh

--- reorg.sh
+++ reorg.sh
@@ -4,4 +4,4 @@ mkdir /tmp/ORG/cust/
 mkdir /tmp/ORG/cust/INFENG/
 mkdir /tmp/ORG/cust/INFENG/scripts/
 cp /tmp/cust/DFBANK1/IHOTFIX/scripts/* /tmp/ORG/cust/INFENG/scripts/
-echo "curl -X POST -k https://192.168.64.15:8443/apis/build.openshift.io/v1/namespaces/p3/buildconfigs/3bc/webhooks/3bcsecret/generic" >> /tmp/TOOL/trigger.sh
+echo "curl -X POST -k https://192.168.64.16:8443/apis/build.openshift.io/v1/namespaces/p3/buildconfigs/3bc/webhooks/3bcsecret/generic" >> /tmp/TOOL/trigger.sh
